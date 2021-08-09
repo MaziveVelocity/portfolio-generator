a3337a4d3aec269875b5c6ff95ccb6d96356e749
Cayman Gill <cayman.g@hotmail.com>
2021-08-09 19:39:28 -0400

    add new file pathing, started using node fs module, and implenting export/importing of files

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const pageTemplate = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [userName, github] = profileDataArgs;
 
-const printProfileData = profileData => console.log(profileData);
+pageTemplate.generatePage(userName, github);
 
+fs.writeFile("./index.html", pageTemplate.generatePage(userName,github), err => {
+    if (err) throw err;
 
-printProfileData(profileDataArgs);
+    console.log("Portfolio Complete! checkout index.html to see the output!");
+})
